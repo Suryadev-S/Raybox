540af2b46a6eb3914ac36f383f6fc1bc7e3bf532
suryadev <somasuryadev6@gmail.com>
2025-10-30 12:31:28 +0530

    feat(api): created RayboxHome fetching logic structure
    - written the trycatch block where /api/raybox is called.
    - faced problem of forwarding of cookies from server to api,
      since new http client is assumed when calling a get request from
      server and hence no cookies are included in the call of server.
      Solved by forwarding the cookies received in the server to the api.

--- src/app/(raybox)/raybox/page.tsx
+++ src/app/(raybox)/raybox/page.tsx
@@ -1,4 +1,24 @@
-const RayboxHome = () => {
+import axios from "axios";
+import { headers } from "next/headers";
+
+const RayboxHome = async () => {
+  let resource: any | null = null;
+  try {
+    const res = await axios.get('http://localhost:3000/api/raybox', {
+      headers: {
+        'Cookie': (await headers()).get("cookie") ?? '',
+      },
+    });
+    if (res.status != 200) {
+      console.error('Error in the api');
+      throw new Error;
+    }
+
+    resource = res.data;
+    console.log(resource);
+  } catch (error) {
+    console.error(error);
+  }
   return (
     <div>
       Raybox home
